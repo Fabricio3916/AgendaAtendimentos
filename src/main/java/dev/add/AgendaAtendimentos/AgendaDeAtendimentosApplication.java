a568aea16021b2af66869d7d4a91fc5112bf7575
Fabricio3916 <fabriciopassos3916@gmail.com>
2025-12-09 10:15:46 -0300

    Primeiro commit

---
+++ src/main/java/dev/add/AgendaAtendimentos/AgendaDeAtendimentosApplication.java
@@ -0,0 +1,13 @@
+package dev.add.AgendaAtendimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaDeAtendimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaDeAtendimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/add/AgendaAtendimentos/AgendaDeAtendimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.add.AgendaAtendimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaDeAtendimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
